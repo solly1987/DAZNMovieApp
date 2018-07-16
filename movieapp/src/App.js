bff46600f0cefb060be7cc4fdd36c8bd7445fca0
Matthew Solly <matthews@tahzoo.com>
2018-07-16 11:58:29 +0100

    created the main view

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -5,6 +5,32 @@ import AppView from "./components/App/View";
 
 class App extends Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            movieDescription: {}
+        };
+
+        this.setMovieDescription = this.setMovieDescription.bind(this);
+    }
+
+    async setMovieDescription(id) {
+        try {
+            this.setState({
+                movieDescription: await getMovie(id)
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    render() {
+        return (
+            <AppView setMovieDescription={this.setMovieDescription}
+                     movieDescription={this.state.movieDescription}/>
+        )
+    }
 }
 
 export default App;
