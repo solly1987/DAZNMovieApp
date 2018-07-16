2690285d6051f6322d86d7b7e800ba9ed215c392
Matthew Solly <matthews@tahzoo.com>
2018-07-16 11:57:23 +0100

    created the containers to handle the movie description and the search bar

--- movieapp/src/containers/MovieDescription.jsx
+++ movieapp/src/containers/MovieDescription.jsx
@@ -7,7 +7,32 @@ import MovieInfo from "../components/MovieDescription/MovieInfo";
 
 const MovieDescription = (props) => {
 
+    const {
+        title, genres, status, runtime, revenue, budget,
+        overview, release_date, production_companies } = props.movieDescription;
 
+    return (
+        <main className="movie-description">
+            <MovieImage
+                title={title}
+                posterPath={props.movieDescription.poster_path}/>
+
+            <MovieInfo
+                title={title}
+                genres={genres}
+                status={status}
+                runtime={runtime}
+                revenue={revenue}
+                budget={budget}
+                overview={overview}
+                releaseDate={release_date}
+                productionCompanies={production_companies}/>
+        </main>
+    )
+};
+
+MovieDescription.propTypes = {
+    movieDescription: PropTypes.object.isRequired
 };
 
 export default MovieDescription;
--- movieapp/src/containers/SearchBar.jsx
+++ movieapp/src/containers/SearchBar.jsx
@@ -9,7 +9,64 @@ import {handleChange} from "../modules/handlers";
 
 class SearchBar extends Component {
 
-   
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchKeyWord: "",
+            suggestionsList: {}
+        };
+
+        this.setMovieSuggestionsList = debounce(500, this.setMovieSuggestionsList).bind(this);
+        this.handleChange = handleChange.bind(this);
+    }
+
+    /*** get movie list on component update ***/
+    componentDidUpdate(nextProps, nextState) {
+        this.setMovieSuggestionsList(nextState.searchKeyWord, response => {
+            this.setState({
+                suggestionsList: response
+            })
+        });
+    }
+
+    /*** set movie list ***/
+    async setMovieSuggestionsList(searchKeyWord, callback) {
+        let data = await getMoviesList(searchKeyWord);
+
+        try {
+            callback(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    render() {
+        return (
+            <div className="search-wrapper">
+                <input type="text"
+                    id="searchKeyWord"
+                    value={this.state.searchKeyWord}
+                    className="search-input"
+                    placeholder="What movie are you looking for??"
+                    searchKeyWord={this.state.searchKeyWord}
+                    onChange={this.handleChange} />
+                <i className="fa fa-search btn" 
+                    aria-hidden="true" 
+                    onClick={this.handleChange} />
+                {Object.keys(this.state.suggestionsList).length > 0 &&
+                this.state.searchKeyWord &&
+                <SearchBarAutocomplete
+                    handleChange={this.handleChange}
+                    setMovieDescription={this.props.setMovieDescription}
+                    suggestionsList={this.state.suggestionsList}/>}
+            </div>
+        )
+    }
 }
+
+SearchBar.propTypes = {
+    setMovieDescription: PropTypes.func.isRequired
+};
 
 export default SearchBar;
